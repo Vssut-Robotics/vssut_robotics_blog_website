c5065155bbadc648b0c1ca00f6940d5a7f29a6f7
Sangram_29 <sangramscience29@gmail.com>
2022-10-08 22:06:23 +0530

    single post component is ready

---
+++ vssut-robotics-blog-app/src/posts/Posts.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Post from '../Post/Post';
+
+
+
+const posts = () => {
+  return (
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+
+    </div>
+  )
+}
+
+export default posts
