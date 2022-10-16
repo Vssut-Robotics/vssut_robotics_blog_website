c41bc2c5962e58ce652d1e14d5e7318c80f1ad8b
Ajit-007 <67113285+Ajit-007@users.noreply.github.com>
2022-10-16 20:05:25 +0530

    Added code for calibrating Temperature Sensor

---
+++ Arduino_projects/temp_sensor.ino
@@ -0,0 +1,27 @@
+int val;
+int tempPin = A0;
+
+void setup()
+{
+  Serial.begin(9600);
+}
+
+
+void loop()
+{
+  val = analogRead(tempPin);
+  float mv = ( val/1024.0)*5000;
+  float cel = mv/10;
+  float farh = (cel*9)/5 + 32;
+  Serial.print("TEMPRATURE = ");
+  Serial.print(cel);
+  Serial.print("*C");
+  Serial.println();
+  delay(1000);
+
+  Serial.print("TEMPRATURE = ");
+  Serial.print(farh);
+  Serial.print("*F");
+  Serial.println();
+
+}
