945db35ccdedd0f61478bdeb309c7738a63d9541
NeoPrint3D <drew@ronsman.com>
2022-10-16 10:09:41 -0500

    style and ui improvements

--- vssut-robotics-blog-app/src/App.js
+++ vssut-robotics-blog-app/src/App.js
@@ -6,27 +6,27 @@ import Write from './pages/Write/Write';
 import SettingsPage from './pages/SettingsPage/SettingsPage';
 import Login from './pages/login/Login';
 import Register from './pages/register/Register';
-import {BrowserRouter as Router, Routes,Route,Link} from 'react-router-dom';
-// import Post from './Post/Post';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import Post from './Post/Post'; 
 
 
 function App() {
   const user = false;
   return (
     <>
-    <Router>
-      <Routes>
-        <Route exact path="/" element={<TopBar />} />
-        <Route path="/home" element={<HomePage />} />
-        <Route path="/register" element={user ? <HomePage /> : <Register />} />
-        <Route path="/login" element={user ? <HomePage /> : <Login />}/>
-        <Route path="/write" element={user ? <Write /> : <Register/>}/>
-        <Route path="/settings" element={user ? <SettingsPage /> : <Register />}/>
-        <Route path="/post/:postId" element={<Single />}/>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<TopBar />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/register" element={user ? <HomePage /> : <Register />} />
+          <Route path="/login" element={user ? <HomePage /> : <Login />} />
+          <Route path="/write" element={user ? <Write /> : <Register />} />
+          <Route path="/settings" element={user ? <SettingsPage /> : <Register />} />
+          <Route path="/post/:postId" element={<Single />} />
 
-      </Routes>
-    </Router>
-     </>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
--- vssut-robotics-blog-app/src/sidebar/Sidebar.jsx
+++ vssut-robotics-blog-app/src/sidebar/Sidebar.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import './Sidebar.css';
+import React from "react";
+import "./Sidebar.css";
 
 const sidebar = () => {
   return (
     <div className="side">
       <div className="sidebarItem">
         <span className="sidebarTitle">ABOUT ME</span>
-        <img src="https://th.bing.com/th/id/OIP.poB1C70nvGVjBWc6yYXFTAHaHa?w=193&h=193&c=7&r=0&o=5&pid=1.7" alt="logo" />
-          <h1>Robotics Society</h1>
-      </div> 
+        <img
+          src="https://th.bing.com/th/id/OIP.poB1C70nvGVjBWc6yYXFTAHaHa?w=193&h=193&c=7&r=0&o=5&pid=1.7"
+          alt="logo"
+        />
+        <h1>Robotics Society</h1>
+      </div>
       <div className="sidebarItem">
         <span className="sidebarTitle">CATEGORIES</span>
         <ul className="sidebarList">
@@ -23,16 +26,13 @@ const sidebar = () => {
         <span className="sidebarTitle">FOLLOW US</span>
         <div className="sidebarSocial">
           <i className="sidebarIcon fab fa-facebook-square"></i>
-          <i className="sidebarIcon fab fa-twitter-square"></i> 
-          <i className="sidebarIcon fab fa-pinterest-square"></i> 
-          <i className="sidebarIcon fab fa-instagram-square"></i> 
-
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
         </div>
       </div>
-
-
     </div>
-  )
-}
+  );
+};
 
-export default sidebar
+export default sidebar;
